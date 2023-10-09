ebf579f1a36ad677e4281664bec07916752c76c8
Цомкалов Игорь <igorcomkalov@gmail.com>
2023-10-09 12:30:15 +0300

    Добавьте файлы проекта.

---
+++ symbol files/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        int maxLen = 0;
+        int curLen = 0;
+
+        using (StreamReader f = new StreamReader("C:\\Users\\igorc\\OneDrive\\Рабочий стол\\24.txt"))
+        {
+            string s = f.ReadToEnd();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'X')
+                {
+                    curLen++;
+                    if (curLen > maxLen)
+                        maxLen = curLen;
+                }
+                else
+                {
+                    curLen = 0;
+                }
+            }
+        }
+
+        Console.WriteLine(maxLen);
+    }
+}
+
+
